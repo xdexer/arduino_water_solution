d64fe6e7c067ad215a909c6063abb8076027f755
xdexer <308167@uwr.edu.pl>
2024-07-04 20:31:58 +0200

    resolve vscode issue

--- constants.hpp
+++ constants.hpp
@@ -2,7 +2,7 @@
 #define CONSTANTS_HPP
 
 // USER CONFIGURATION CONSTANTS
-#define DEBUG 1       // TURN ON DEBUG LOGS AND MINIMIZE SLEEP TIMES
+#define DEBUG 0       // TURN ON DEBUG LOGS AND MINIMIZE SLEEP TIMES
 #define THRESHOLD 400 // minimum moisture level that is classified as suitable
 #define RELAY_PIN 4
 #define MOISTURE_PIN A0
